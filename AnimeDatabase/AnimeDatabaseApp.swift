d6a76a394eafa73fd2eea10e9573a7659c6efa2f
UMARU-xiaomai <UMARU2017@163.com>
2024-02-27 23:13:13 +0800

    Initial Commit

---
+++ AnimeDatabase/AnimeDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeDatabaseApp.swift
+//  AnimeDatabase
+//
+//  Created by ShimitsuKoi on 2024/2/27.
+//
+
+import SwiftUI
+
+@main
+struct AnimeDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeDatabase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimeDatabase
+//
+//  Created by ShimitsuKoi on 2024/2/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
